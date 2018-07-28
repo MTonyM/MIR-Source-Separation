0f597eb06c585d4962e6851553b384fe2939f604
MTonyM <maoymt@gmail.com>
2018-07-28 21:58:27 +0800

    update datasets/dataset.py

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -6,11 +6,11 @@ from audio_op import *
 
 data_root = "/home/maoym/MIR-separation/DL_monaural_separation/pyTorch_version/data"
 
-class iKala(Dataset):
+class data(Dataset):
     def __init__(self, args):
         self.len_frame = args.len_frame
         self.len_hop = args.len_hop
-        self.spec_list = torch.load(data_root + "/spec/" + args.dataset + "_spec_f%d_h%d.pth" % (self.len_frame, self.len_hop))["iKala_specs"]
+        self.spec_list = torch.load(data_root + "/spec/" + args.dataset + "_spec_f%d_h%d.pth" % (self.len_frame, self.len_hop))
 
     def __getitem__(self, idx):
         # load file
@@ -24,24 +24,6 @@ class iKala(Dataset):
         return len(self.spec_list)
 
 
-class iKala_aug(Dataset):
-    def __init__(self, args):
-        self.len_frame = args.len_frame
-        self.len_hop = args.len_hop
-        self.spec_list = torch.load(data_root + "/spec/iKala_spec_f%d_h%d_aug.pth" % (self.len_frame, self.len_hop))["iKala_specs"]
-        
-    def __getitem__(self, idx):
-        data = torch.load(self.spec_list[idx])
-        song_mag, voice_mag, mixed_mag, phase = data['song_mag'], data['voice_mag'], data['mixed_mag'], data['mixed_phase']
-        mixed_mag_pre = torch.load(self.spec_list[max(idx-1,0)])['mixed_mag']
-        mixed_mag_next = torch.load(self.spec_list[min(idx,len(self.spec_list)-1)])['mixed_mag']
-        input_mag = np.concatenate((mixed_mag_pre, mixed_mag, mixed_mag_next), axis=1)
-        target_mag = np.concatenate((song_mag, voice_mag), axis=1)
-        return input_mag, target_mag, phase
-    
-    def __len__(self):
-        return len(self.spec_list)
-
 
 # class Wav_aug(iKala_aug):
 #     def __init__(self, frame, hop):
@@ -51,5 +33,5 @@ class iKala_aug(Dataset):
 
 
 def get_dataloader(args):
-    return eval(args.dataset + '(args)')
+    return eval(data + '(args)')
 
